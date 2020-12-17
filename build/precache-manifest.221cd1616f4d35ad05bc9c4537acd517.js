aa1e3dd82c090c63aad881a5939dad8539d4a42f
gustavohdev <g_a93@outlook.com>
2020-12-17 01:33:50 -0300

    first commit

---
+++ build/precache-manifest.221cd1616f4d35ad05bc9c4537acd517.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9357c8ff371c4aef81ddef4bdb6d131",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f2592ad3f555528381e",
+    "url": "/static/css/2.e8fed143.chunk.css"
+  },
+  {
+    "revision": "c70fad1ff50d27fc666a",
+    "url": "/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "6f2592ad3f555528381e",
+    "url": "/static/js/2.dc313092.chunk.js"
+  },
+  {
+    "revision": "c70fad1ff50d27fc666a",
+    "url": "/static/js/main.267d32b9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "076dc20edf52be6efbb83c7dd09b84fa",
+    "url": "/static/media/map-hue.076dc20e.png"
+  },
+  {
+    "revision": "b01e6120d05abd33918d1dbb78c0660f",
+    "url": "/static/media/map-saturation-overlay.b01e6120.png"
+  }
+]);
